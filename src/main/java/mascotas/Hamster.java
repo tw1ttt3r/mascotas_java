a00391c379a538fbb12ea56ada66621d368fdf6d
juanmcardona <juanm.cardona@gmail.com>
2025-10-04 11:33:01 -0600

    Agregar info

--- src/main/java/mascotas/Hamster.java
+++ src/main/java/mascotas/Hamster.java
@@ -8,3 +8,34 @@ public class Hamster {
     String nombre = "";
     double peso = 0.0;
     double longitud = 0.0;
+
+    public Hamster(String nombre, String raza, char genero, int edad, double peso, double longitud) {
+        this.nombre = nombre;
+        this.raza = raza;
+        this.genero = genero;
+        this.edad = edad;
+        this.peso = peso;
+        this.longitud = longitud;
+    }
+
+    public void comer() {
+    }
+
+    public void correr() {
+    }
+
+    public void dormir() {
+    }
+
+    public void beber() {
+    }
+
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Genero: " + genero);
+        System.out.println("Edad: " + edad);
+        System.out.println("Peso: " + peso);
+        System.out.println("Longitud: " + longitud);
+    }
+}
