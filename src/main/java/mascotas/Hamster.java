fe98aed3406e3ec267901d653dedaaa2f5c59525
juanmcardona <juanm.cardona@gmail.com>
2025-10-04 11:14:44 -0600

    hamster-juan

--- src/main/java/mascotas/Hamster.java
+++ src/main/java/mascotas/Hamster.java
@@ -1,4 +1,10 @@
 package mascotas;
 
 public class Hamster {
-}
+    String color = "";
+    int edad = 0;
+    String raza = "";
+    char genero = ' ';
+    String nombre = "";
+    double peso = 0.0;
+    double longitud = 0.0;
