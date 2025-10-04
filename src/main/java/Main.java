a5dddf6d443be0820ebb3bf38d30fec7f69a973b
Angel Gonzalez <angel.glez.g@gmail.com>
2025-10-04 11:13:20 -0600

    feature: inicializa clase lince.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import mascotas.Lince;
+
 public class Main {
     public static void main(String[] args) {
+
+        Lince lince1 = new Lince("Zod", "Moteado", "Lynx", 'M', 10, 30, 105);
 
     }
 }
--- src/main/java/mascotas/Lince.java
+++ src/main/java/mascotas/Lince.java
@@ -1,4 +1,39 @@
 package mascotas;
 
 public class Lince {
+
+    private String nombre = "";
+    private String especie = "";
+    private String nombreCientifico = "";
+    private char genero = ' ';
+    private int edad = 0;
+    private double peso = 0.0;
+    private double longitud = 0.0;
+
+    public Lince(String nombre, String especie, String nombreCientifico, char genero, int edad, double peso, double longitud) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.nombreCientifico = nombreCientifico;
+        this.genero = genero;
+        this.edad = edad;
+        this.peso = peso;
+        this.longitud = longitud;
+    }
+
+    public void cazar(){}
+
+    public void saltar(){}
+
+    public void dormir(){}
+
+    public  void comer(){}
+
+    public void mostrarInfo() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Nombre Cientifico: " + nombreCientifico);
+        System.out.println("Genero: " + genero);
+        System.out.println("Edad: " + edad);
+        System.out.println("Peso: " + peso);
+        System.out.println("Longitud: " + longitud);
+    }
 }
